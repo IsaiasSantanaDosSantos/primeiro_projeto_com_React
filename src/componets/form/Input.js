f0159e730b9e67071ecfcc68bdf025eb97e89dd6
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-02-03 20:01:17 -0300

    Passando mensagem via API

--- src/componets/form/Input.js
+++ src/componets/form/Input.js
@@ -1,11 +1,18 @@
 import styles from './Input.module.css'
 
-function Input({type, text, name, placeholder, handleOnChanger, value}) {
+function Input({type, text, name, placeholder, handleOnChange, value}) {
 
     return(
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <input type={type} name={name} id={name} placeholder={placeholder} onChange={handleOnChanger} value={value} />
+            <input 
+            type={type} 
+            name={name} 
+            id={name} 
+            placeholder={placeholder} 
+            onChange={handleOnChange} 
+            value={value} 
+            />
         </div>
     )
 }
